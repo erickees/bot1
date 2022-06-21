48acff2d45f5f63133a9cb6b91c4f417d67a12a6
erickees <100990778+erickees@users.noreply.github.com>
2022-06-20 23:39:38 -0400

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,6 @@
+import os
+
+api_id = os.environ.get('11330931')
+api_hash = os.environ.get('f72fa71b56550c03e94e16a280841c5f')
+bot_token = os.environ.get('5253599287:AAEM_bpW9Cwc_rxBZiiZlJoOXtiyanmwAP0')
+bot_admin_group = os.environ.get('erickees')
